63d24f145dded881e6746e1afa65b630db652aac
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-18 00:07:41 +0300

    declare department class and assign it's values

--- src/main/java/models/Department.java
+++ src/main/java/models/Department.java
@@ -1,4 +1,16 @@
 package models;
 
+import java.util.Objects;
+
 public class Department {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Department(int id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
 }
