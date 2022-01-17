b99859c30e957358d7f306866a17bf315f01fe7d
AdamuWanganya <wanganyaadamu@gmail.com>
2022-01-18 00:43:09 +0300

    declare and assign values in user

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,4 +1,19 @@
 package models;
 
+import java.util.Objects;
+
 public class User {
+    private int id;
+    private String name;
+    private String position;
+    private String role;
+    private int departmentId;
+
+    public User(int id, String name, String position, String role, int departmentId) {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.role = role;
+        this.departmentId = departmentId;
+    }
 }
